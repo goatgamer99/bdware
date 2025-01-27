4a9cf94c816824e124fda65d5e62ff13485b6bc4
goatgamer99 <6uggedsalt@proton.me>
2025-01-27 05:57:57 -0500

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -454,3 +454,4 @@ ThemeManager:ApplyToTab(Tabs['UI Settings'])
 -- You can use the SaveManager:LoadAutoloadConfig() to load a config
 -- which has been marked to be one that auto loads!
 SaveManager:LoadAutoloadConfig()
+print("test")
